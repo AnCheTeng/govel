d9d5505967735f318129c7fb0e5d167524dc4a84
anche.teng <anche.teng@appier.com>
2020-11-23 21:08:40 +0800

    Get port from env

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/AnCheTeng/govel/routers"
+import (
+	"os"
+
+	"github.com/AnCheTeng/govel/routers"
+)
 
 func main() {
 	r := routers.InitRouter()
-	r.Run(":7788")
+	r.Run(":" + os.Getenv("PORT"))
 }
