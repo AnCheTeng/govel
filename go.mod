875dab054bf6c61199f8bcca009f860624432c1b
anche.teng <anche.teng@appier.com>
2020-12-05 22:25:27 +0800

    CORS support

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/PuerkitoBio/goquery v1.6.0
 	github.com/gin-gonic/gin v1.6.3
 	github.com/pkg/errors v0.9.1
+	github.com/rs/cors v1.7.0
 	github.com/sirupsen/logrus v1.7.0
 	golang.org/x/text v0.3.2
 	gopkg.in/yaml.v2 v2.2.8
--- routers/router.go
+++ routers/router.go
@@ -1,12 +1,16 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	cors "github.com/rs/cors/wrapper/gin"
+)
 
 // InitRouter initialize routing information
 func InitRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(cors.Default())
 
 	APIv1 := r.Group("/api/v1")
 	{
